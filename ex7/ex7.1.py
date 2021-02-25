43d6d92102ffbf85c406af30bf4db05cf6cb0a4c
wlbailey0 <wlbailey0@gmail.com>
2021-02-24 22:05:06 -0600

    I made a mistake

--- ex7/ex7.1.py
+++ ex7/ex7.1.py
@@ -45,7 +45,7 @@ def mysqrt(a):
             break
         x = estimated_root
 
-def test_square_root(list_of_a):
+def test_square_root():
     """Displays outcomes of calculating square root of a using different methods;
     list_of_a - list of positive digit.
     """
@@ -61,4 +61,4 @@ def test_square_root(list_of_a):
             print("{:<3} {:<13.11} {:<13.11} {:<10.11}".format(i, mysqrt(i), math.sqrt(i), diff)
 
 
-test_square_root(1,10)
+test_square_root()
